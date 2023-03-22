ae0e5a8468b9606f5849bd46b183473a22fd121b
Sebastian Zhao <78707369+sebzhao@users.noreply.github.com>
2023-03-22 16:11:11 -0700

    Add in more pseudocode for main.py

--- main.py
+++ main.py
@@ -6,19 +6,92 @@ import argparse
 
 def main():
 
+    img_path = '~/data/1.png'
+
     #FIXME: Run model inference on original image
+
+    for i in range(numgens):
+        with torch.no_grad():
+            outputs = model.sample(image_tensor, greedy=greedy[i], 
+                                   temperature=temperature, beam=beam[i], true_ingrs=None)
+            
+        ingr_ids = outputs['ingr_ids'].cpu().numpy()
+        recipe_ids = outputs['recipe_ids'].cpu().numpy()
+            
+        outs, valid = prepare_output(recipe_ids[0], ingr_ids[0], ingrs_vocab, vocab)
+        
+        if valid['is_valid'] or show_anyways:
+            
+            print ('RECIPE', num_valid)
+            num_valid+=1
+            #print ("greedy:", greedy[i], "beam:", beam[i])
+    
+            BOLD = '\033[1m'
+            END = '\033[0m'
+            print (BOLD + '\nTitle:' + END,outs['title'])
+
+            print (BOLD + '\nIngredients:'+ END)
+            print (', '.join(outs['ingrs']))
+
+            print (BOLD + '\nInstructions:'+END)
+            print ('-'+'\n-'.join(outs['recipe']))
+
+            print ('='*20)
+
+        else:
+            pass
+            print ("Not a valid recipe!")
+            print ("Reason: ", valid['reason'])
+
+    igr = outs['ingrs']
 
 
     #FIXME: Create hardcoded StableDiffusion prompt
 
+    prompt = "Fancy + " + ingr + "alisdhfjklsajdg"
 
     #FIXME: Use StableDiffusion to generate new image
 
+    new_image = StableDiffusionPipeline(prompt, image)
 
     #FIXME: Run model inference on new image
+
+    for i in range(numgens):
+        with torch.no_grad():
+            outputs = model.sample(image_tensor, greedy=greedy[i], 
+                                   temperature=temperature, beam=beam[i], true_ingrs=None)
+            
+        ingr_ids = outputs['ingr_ids'].cpu().numpy()
+        recipe_ids = outputs['recipe_ids'].cpu().numpy()
+            
+        outs, valid = prepare_output(recipe_ids[0], ingr_ids[0], ingrs_vocab, vocab)
+        
+        if valid['is_valid'] or show_anyways:
+            
+            print ('RECIPE', num_valid)
+            num_valid+=1
+            #print ("greedy:", greedy[i], "beam:", beam[i])
+    
+            BOLD = '\033[1m'
+            END = '\033[0m'
+            print (BOLD + '\nTitle:' + END,outs['title'])
+
+            print (BOLD + '\nIngredients:'+ END)
+            print (', '.join(outs['ingrs']))
+
+            print (BOLD + '\nInstructions:'+END)
+            print ('-'+'\n-'.join(outs['recipe']))
+
+            print ('='*20)
+
+        else:
+            pass
+            print ("Not a valid recipe!")
+            print ("Reason: ", valid['reason'])
 
 
     #FIXME: Print out output/display new image onto terminal
 
+    
 
     pass
