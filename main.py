96cc72ab824c799df060e6fd72e3fc4be8baf307
Annabelle Park <115906717+annbypark@users.noreply.github.com>
2023-03-23 00:04:06 -0700

    everything till terminal display

--- main.py
+++ main.py
@@ -1,12 +1,76 @@
+import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
 import numpy as np
-import argparse
+import os
+from args import get_parser
+import pickle
+from model import get_model
+from torchvision import transforms
+from utils.output_utils import prepare_output
+from PIL import Image
+import time
+from diffusers import StableDiffusionPipeline
 
 #FIXME: Set up argparser + arguments
 
+parser.add_command(parser, "-i", "--image", metavar="IMAGE_FLIP", help = "Path to your input image")
+
 def main():
 
-    img_path = '~/data/1.png'
+    data_dir = '../data' #path to vocab and model checkpoint
+
+    use_gpu = False #running on gpu or cpu
+    device = torch.device('cuda' if torch.cuda.is_available() and use_gpu else 'cpu')
+    map_loc = None if torch.cuda.is_available() and use_gpu else 'cpu'
+
+
+    ingrs_vocab = pickle.load(open(os.path.join(data_dir, 'ingr_vocab.pkl'), 'rb'))
+    vocab = pickle.load(open(os.path.join(data_dir, 'instr_vocab.pkl'), 'rb'))
+
+    ingr_vocab_size = len(ingrs_vocab)
+    instrs_vocab_size = len(vocab)
+
+    t = time.time()
+    import sys; sys.argv=['']; del sys
+    args = get_parser()
+    args.maxseqlen = 15
+    args.ingrs_only=False
+    model = get_model(args, ingr_vocab_size, instrs_vocab_size)
+    # Load the trained model parameters
+    model_path = os.path.join(data_dir, 'modelbest.ckpt')
+    model.load_state_dict(torch.load(model_path, map_location=map_loc))
+    model.to(device)
+    model.eval()
+    model.ingrs_only = False
+    model.recipe_only = False
+
+    image_path = args['image'] #inputted image path
+    image = Image.open(image_path).convert('RGB') #image
+
+    transf_list_batch = []             #variables 
+    transf_list_batch.append(transforms.ToTensor())
+    transf_list_batch.append(transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)))
+    to_input_transf = transforms.Compose(transf_list_batch)
+
+    greedy = [True, False, False, False]
+    beam = [-1, -1, -1, -1]
+    temperature = 1.0
+    numgens = len(greedy)
+
+    transf_list = []
+    transf_list.append(transforms.Resize(256))
+    transf_list.append(transforms.CenterCrop(224))
+    transform = transforms.Compose(transf_list)
+
+    image_transf = transform(image)
+    image_tensor = to_input_transf(image_transf).unsqueeze(0).to(device)
+
+    plt.imshow(image_transf)
+    plt.axis('off')
+    plt.show()
+    plt.close()
+    num_valid = 1
 
     #FIXME: Run model inference on original image
 
@@ -20,45 +84,28 @@ def main():
             
         outs, valid = prepare_output(recipe_ids[0], ingr_ids[0], ingrs_vocab, vocab)
         
-        if valid['is_valid'] or show_anyways:
-            
-            print ('RECIPE', num_valid)
-            num_valid+=1
-            #print ("greedy:", greedy[i], "beam:", beam[i])
-    
-            BOLD = '\033[1m'
-            END = '\033[0m'
-            print (BOLD + '\nTitle:' + END,outs['title'])
-
-            print (BOLD + '\nIngredients:'+ END)
-            print (', '.join(outs['ingrs']))
-
-            print (BOLD + '\nInstructions:'+END)
-            print ('-'+'\n-'.join(outs['recipe']))
-
-            print ('='*20)
-
-        else:
-            pass
-            print ("Not a valid recipe!")
-            print ("Reason: ", valid['reason'])
-
-    igr = outs['ingrs']
+        recipe_name = outs['title']
+        ingredients = outs['ingrs']
 
 
     #FIXME: Create hardcoded StableDiffusion prompt
 
-    prompt = "Fancy + " + ingr + "alisdhfjklsajdg"
+    prompt = "Fancy food plating of " + recipe_name + " with ingredients " + ingredients
 
     #FIXME: Use StableDiffusion to generate new image
 
-    new_image = StableDiffusionPipeline(prompt, image)
+    pipe = StableDiffusionPipeline.from_pretrained('CompVis/stable-diffusion-v1-4').to('cuda')
+
+    new_image = pipe(prompt).images[0]
 
     #FIXME: Run model inference on new image
 
+    image_transf_new = transform(new_image)
+    image_tensor_new = to_input_transf(image_transf_new).unsqueeze(0).to(device)
+
     for i in range(numgens):
         with torch.no_grad():
-            outputs = model.sample(image_tensor, greedy=greedy[i], 
+            outputs = model.sample(image_tensor_new, greedy=greedy[i], 
                                    temperature=temperature, beam=beam[i], true_ingrs=None)
             
         ingr_ids = outputs['ingr_ids'].cpu().numpy()
@@ -66,32 +113,10 @@ def main():
             
         outs, valid = prepare_output(recipe_ids[0], ingr_ids[0], ingrs_vocab, vocab)
         
-        if valid['is_valid'] or show_anyways:
-            
-            print ('RECIPE', num_valid)
-            num_valid+=1
-            #print ("greedy:", greedy[i], "beam:", beam[i])
-    
-            BOLD = '\033[1m'
-            END = '\033[0m'
-            print (BOLD + '\nTitle:' + END,outs['title'])
-
-            print (BOLD + '\nIngredients:'+ END)
-            print (', '.join(outs['ingrs']))
-
-            print (BOLD + '\nInstructions:'+END)
-            print ('-'+'\n-'.join(outs['recipe']))
-
-            print ('='*20)
-
-        else:
-            pass
-            print ("Not a valid recipe!")
-            print ("Reason: ", valid['reason'])
+        recipe_name_new = outs['title']
+        ingredients_new = outs['ingrs']
 
 
     #FIXME: Print out output/display new image onto terminal
 
     
-
-    pass
